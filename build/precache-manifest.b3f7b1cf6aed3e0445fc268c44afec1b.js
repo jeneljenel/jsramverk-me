668f62d8a7e64af5cac2b606aafc0307dab0d242
jeneljenel <jwwlee@gmail.com>
2019-10-10 15:45:37 +0200

    added build

---
+++ build/precache-manifest.b3f7b1cf6aed3e0445fc268c44afec1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf2a4ff23405eadadfb32e0e6bc4640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd0abbb3b1291b7459ca",
+    "url": "/static/css/main.586ef367.chunk.css"
+  },
+  {
+    "revision": "fe05b883b96394776c99",
+    "url": "/static/js/2.e336dd8d.chunk.js"
+  },
+  {
+    "revision": "bd0abbb3b1291b7459ca",
+    "url": "/static/js/main.eebb775e.chunk.js"
+  },
+  {
+    "revision": "2f105c8c42093b824d78",
+    "url": "/static/js/runtime-main.d4cb1601.js"
+  }
+]);
